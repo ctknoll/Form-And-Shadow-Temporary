8e3afdd88730070e892424603f3b32ee81712d4c
DanielColina <voluuna@gmail.com>
2017-02-13 22:30:54 -0600

    Move Cube changes and Checkpoint Prefab
    -More tweaks to movement cube
    -Implemented checkpoint prefab

---
+++ Form and Shadow/Assets/Scripts/Game Control/Checkpoint.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            PlayerMovement.playerStartPosition = transform.position;
+        }
+    }
+}
