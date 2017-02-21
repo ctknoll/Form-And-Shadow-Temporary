1c376780137b8dbe6b2fa49eace0d97bd2324135
silverarcaine <jsm140930@utdallas.edu>
2017-02-21 11:00:29 -0600

    Hundreds of Changes regarding Level 2 and Build 6

--- Form and Shadow/Assets/Scripts/Dynamic and Moving Objects/Switches/PressureSwitches/MovingPlatformSpeedPressureSwitch.cs
+++ Form and Shadow/Assets/Scripts/Dynamic and Moving Objects/Switches/PressureSwitches/MovingPlatformSpeedPressureSwitch.cs
@@ -5,6 +5,7 @@ public class MovingPlatformSpeedPressureSwitch : PressureSwitch
 
 	public GameObject movingBlock;
 	public float percentSlow;
+	public bool haltWhenNotPressed;
 	private bool locked;
 
 	// Use this for initialization
@@ -17,6 +18,7 @@ public class MovingPlatformSpeedPressureSwitch : PressureSwitch
 	public void Update () 
 	{
         //base.Update();
+		if (haltWhenNotPressed) movingBlock.GetComponent<MovingPlatform>().slowValue = 0;
         if (pressed) 
 		{
 			if (movingBlock.GetComponent<MovingPlatform> () != null) 
