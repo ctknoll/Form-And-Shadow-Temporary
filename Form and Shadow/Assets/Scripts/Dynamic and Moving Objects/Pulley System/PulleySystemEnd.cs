8cf087982981ab4485e91a60dcf3a4a79249f0cb
DanielColina <voluuna@gmail.com>
2017-02-21 13:28:43 -0600

    Cleaning master

--- Form and Shadow/Assets/Scripts/Dynamic and Moving Objects/Pulley System/PulleySystemEnd.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class PulleySystemEnd : MonoBehaviour {
-	void OnTriggerEnter(Collider other)
-	{
-		if(other.gameObject.tag == "Move Platform")
-			other.gameObject.transform.parent.GetComponent<PulleySystemPlatform>().atEndOfRoute = true;
-	}
-}
