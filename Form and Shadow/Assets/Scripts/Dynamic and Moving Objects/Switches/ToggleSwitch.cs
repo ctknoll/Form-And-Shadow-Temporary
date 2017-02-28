9cee3fc52abbbd6443c647fa65fc90937759fa02
DanielColina <voluuna@gmail.com>
2017-02-27 22:38:53 -0600

    Switch Fix
    -Temporary fix to toggle switches to make them work tomorrow, need to be
    rewritten for efficiency.

--- Form and Shadow/Assets/Scripts/Dynamic and Moving Objects/Switches/ToggleSwitch.cs
+++ Form and Shadow/Assets/Scripts/Dynamic and Moving Objects/Switches/ToggleSwitch.cs
@@ -2,21 +2,28 @@
 using System.Collections;
 
 public class ToggleSwitch : MonoBehaviour {
-
-	public bool pressed;
     public GameObject leverArm;
-    public float pressAnimationTime;
-	public float timerDuration;
-    public AudioSource timerAudioSource;
-    public AudioSource switchAudioSource;
 
+    public bool timerToggleSwitch;
+    public bool lerpToggleSwitch;
+    public float timerDuration;
+
+    public bool pressed;
+    public float switchFlipAnimationTime;
+    public AudioSource timerAudioSource;
+    public AudioSource switchFlipAudioSource;
+
+    private bool animating;
+    private float runningTime = 0;
+    private bool toggledOn;
     private Vector3 startLerpRotation;
     private Vector3 endLerpRotation;
-	public float runningTime = 0;
 
 
 	public void Start()
 	{
+        animating = false;
+        toggledOn = false;
         pressed = false;
         startLerpRotation = leverArm.transform.eulerAngles;
         endLerpRotation = leverArm.transform.eulerAngles + new Vector3(0, 0, 90);
@@ -27,28 +34,27 @@ public class ToggleSwitch : MonoBehaviour {
     {
         if(other.gameObject.tag == "Player" && !PlayerMovement.shadowMelded && !PlayerMovement.shadowShiftingIn && !PlayerMovement.shadowShiftingOut)
         {
-			if (!pressed) {
+            if(!animating)
+            {
                 GameController.ToggleInteractTooltip(true);
-				if (Input.GetButtonDown ("Grab")) 
-				{
-					pressed = true;
-					runningTime = timerDuration;
-					StartCoroutine (PressSwitch ());
-                    if (timerDuration >= 0) StartCoroutine(DepressSwitch());
-					GameController.ToggleInteractTooltip (false);
-				}
-			}
-            //else
-            //{
-            //    if (Input.GetButtonDown("Grab"))
-            //    {
-            //        pressed = false;
-            //        GetComponent<AudioSource>().Stop();
-            //        runningTime = 0;
-            //        StartCoroutine(DepressSwitch());
-            //        GameController.ToggleInteractTooltip(false);
-            //    }
-            //}
+                if (Input.GetButtonDown("Grab"))
+                {
+                    if (timerToggleSwitch)
+                    {
+                        pressed = true;
+                        runningTime = timerDuration;
+                        StartCoroutine(PressSwitchTimer());
+                        StartCoroutine(DepressSwitchTimer());
+                        GameController.ToggleInteractTooltip(false);
+                    }
+                    else
+                    {
+                        pressed = true;
+                        StartCoroutine(PressSwitchToggle());
+                        GameController.ToggleInteractTooltip(false);
+                    }
+                }
+            }
         }
     }
 
@@ -68,37 +74,64 @@ public class ToggleSwitch : MonoBehaviour {
 		} 
 		else if (runningTime <= 0 && runningTime > -1)
 		{
-			pressed = false;
             runningTime = 0;
 		}
 	}
 
-    public IEnumerator PressSwitch()
+    public IEnumerator PressSwitchTimer()
     {
+        switchFlipAudioSource.Play();
+        animating = true;
         float panStart = Time.time;
-
-        switchAudioSource.Play();
-
-        while (Time.time < panStart + pressAnimationTime)
+        while (Time.time < panStart + switchFlipAnimationTime)
         {
-            leverArm.transform.eulerAngles = Vector3.Lerp(startLerpRotation, endLerpRotation, (Time.time - panStart) / pressAnimationTime);
+            leverArm.transform.eulerAngles = Vector3.Lerp(startLerpRotation, endLerpRotation, (Time.time - panStart) / switchFlipAnimationTime);
             yield return null;
         }
     }
 
-    public IEnumerator DepressSwitch()
+    public IEnumerator PressSwitchToggle()
+    {
+        switchFlipAudioSource.Play();
+        animating = true;
+        float panStart = Time.time;
+        while (Time.time < panStart + switchFlipAnimationTime)
+        {
+            if (!toggledOn)
+                leverArm.transform.eulerAngles = Vector3.Lerp(startLerpRotation, endLerpRotation, (Time.time - panStart) / switchFlipAnimationTime);
+            else
+                leverArm.transform.eulerAngles = Vector3.Lerp(endLerpRotation, startLerpRotation, (Time.time - panStart) / switchFlipAnimationTime);
+            yield return null;
+        }
+        if (!toggledOn)
+            toggledOn = true;
+        else
+            toggledOn = false;
+        if (lerpToggleSwitch)
+        {
+            if (!toggledOn)
+                pressed = false;
+        }
+        else
+            pressed = false;
+        animating = false;
+    }
+
+    public IEnumerator DepressSwitchTimer()
     {
         timerAudioSource.Play();
+        animating = true;
         if (timerDuration >= 0) yield return new WaitForSeconds(timerDuration);
-        switchAudioSource.Play();
+        switchFlipAudioSource.Play();
         timerAudioSource.Stop();
 
         float panStart = Time.time;
-        while (Time.time < panStart + pressAnimationTime)
+        while (Time.time < panStart + switchFlipAnimationTime)
         {
-            leverArm.transform.eulerAngles = Vector3.Lerp(endLerpRotation, startLerpRotation, (Time.time - panStart) / pressAnimationTime);
+            leverArm.transform.eulerAngles = Vector3.Lerp(endLerpRotation, startLerpRotation, (Time.time - panStart) / switchFlipAnimationTime);
             yield return null;
         }
         pressed = false;
+        animating = false;
     }
 }
