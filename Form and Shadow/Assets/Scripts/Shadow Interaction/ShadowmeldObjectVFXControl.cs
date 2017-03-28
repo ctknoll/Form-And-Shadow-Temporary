dd291b052718adc65d59d6aabde18170c50a7f8b
DanielColina <voluuna@gmail.com>
2017-03-28 13:08:53 -0500

    Torch Further Implementation
    -Further updated torch mechanic to now cause 3D objects to shadowmeld
    with the player.

---
+++ Form and Shadow/Assets/Scripts/Shadow Interaction/ShadowmeldObjectVFXControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ShadowmeldObjectVFXControl : MonoBehaviour {
+	void Start ()
+    {
+        transform.localScale = new Vector3(1, 1, 1);
+        transform.localPosition = new Vector3(0, 0, 0);
+        SetParticleSettings();
+	}
+
+    void Update()
+    {
+        if(PlayerMovement.shadowMelded)
+        {
+            GetComponent<ParticleSystem>().Play();
+        }
+        else
+        {
+            GetComponent<ParticleSystem>().Clear();
+            GetComponent<ParticleSystem>().Stop();
+        }
+    }
+    void SetParticleSettings ()
+    {
+        ParticleSystem.ShapeModule shapeMod = GetComponent<ParticleSystem>().shape;
+        shapeMod.mesh = GetComponentInParent<MeshFilter>().mesh;
+
+        ParticleSystem.EmissionModule emissionMod = GetComponent<ParticleSystem>().emission;
+        float[] parentScales = new float[] { transform.parent.lossyScale.x, transform.parent.lossyScale.y, transform.parent.lossyScale.z };
+        emissionMod.rateOverTime = 500 * Mathf.Max(parentScales);
+	}
+}
--- Form and Shadow/Assets/Scripts/Shadow Interaction/TorchLightControl.cs
+++ Form and Shadow/Assets/Scripts/Shadow Interaction/TorchLightControl.cs
@@ -1,16 +1,43 @@
 ﻿using UnityEngine;
 
 public class TorchLightControl : MonoBehaviour {
+    public GameObject shadowMeldObjectVFXPrefab;
 
     void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Shadow"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Shadow") && other.gameObject.tag != "Player")
+        {
             other.gameObject.layer = LayerMask.NameToLayer("Invisible Shadow");
+            // Handle the logic of moving the 3D object to the shadowmeld ignore layer
+            if (other.GetComponent<ShadowCollider>().exceptionParent)
+            {
+                other.GetComponent<ShadowCollider>().exceptionParent.layer = LayerMask.NameToLayer("Shadowmeld Ignore");
+                Instantiate(shadowMeldObjectVFXPrefab, other.GetComponent<ShadowCollider>().exceptionParent.transform);
+            }
+            else
+            {
+                other.transform.parent.gameObject.layer = LayerMask.NameToLayer("Shadowmeld Ignore");
+                Instantiate(shadowMeldObjectVFXPrefab, other.transform.parent);
+            }
+        }
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.gameObject.layer == LayerMask.NameToLayer("Invisible Shadow"))
+        {
             other.gameObject.layer = LayerMask.NameToLayer("Shadow");
+            // Handle the logic of moving the 3D object back to the original layer
+            if (other.GetComponent<ShadowCollider>().exceptionParent)
+            {
+                other.GetComponent<ShadowCollider>().exceptionParent.layer = LayerMask.NameToLayer("Form");
+                Destroy(other.GetComponent<ShadowCollider>().exceptionParent.transform.FindChild("ShadowmeldObjectVFX(Clone)").gameObject);
+            }
+            else
+            {
+                other.transform.parent.gameObject.layer = LayerMask.NameToLayer("Form");
+                Destroy(other.transform.parent.FindChild("ShadowmeldObjectVFX(Clone)").gameObject);
+            }
+        }
     }
 }
