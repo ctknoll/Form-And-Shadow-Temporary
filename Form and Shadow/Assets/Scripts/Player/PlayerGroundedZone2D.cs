4f3204ba96bb22d3f10ffdeb4e44e57e6b64e1eb
DanielColina <voluuna@gmail.com>
2017-04-03 17:16:04 -0500

    Sounds, Fixes, and Final Level Pass
    -All sounds (that are known of), are in. This includes Player movement
    (step, jump, land), ambient effects (background gears, background
    shadowmeld), shadowmeld/shift exit/enter, switch sounds (pressure and
    toggle), conveyor belt ambience, and player death.
    -Various fixes across levels with specific prefab changes that needed to
    be normalized.
    -Created a new prefab, Dead_Player, and replaced all dead players across
    the levels that now properly go invisible when the player is
    shadowshifted.
    -Made shadowshift tooltip now only appear when the player can actually
    shadowshift.
    -Made checkpoint no longer do wonky shit with their transform lerp when
    triggered.

--- Form and Shadow/Assets/Scripts/Player/PlayerGroundedZone2D.cs
+++ Form and Shadow/Assets/Scripts/Player/PlayerGroundedZone2D.cs
@@ -2,7 +2,26 @@
 
 public class PlayerGroundedZone2D : MonoBehaviour{
 
-	void OnTriggerStay(Collider other)
+    PlayerMovement playerMovement;
+
+    void Start()
+    {
+        playerMovement = GameObject.Find("Player_Character").GetComponent<PlayerMovement>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!PlayerMovement.grounded2D && !PlayerMovement.shadowShiftingIn && !PlayerMovement.shadowShiftingOut)
+        {
+            if (other.GetComponent<Collider>().isTrigger != true && other.gameObject.tag != "Player")
+            {
+                playerMovement.playerAudioSource.clip = playerMovement.jumpLandClip;
+                playerMovement.playerAudioSource.Play();
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider other)
 	{
 		if(other.GetComponent<Collider>().isTrigger != true)
 		    PlayerMovement.grounded2D = true;
